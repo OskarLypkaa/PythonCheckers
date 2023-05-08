a0de5500072518d87c1cbab10f7f057ed9eb426a
Oskar <oskar.lypka@gmail.com>
2023-05-08 21:27:17 +0200

    Creating basic logic for pieces

---
+++ pieces.py
@@ -0,0 +1,24 @@
+
+from board import Checkerboard
+from players import HumanPlayer, ComputerPlayer
+
+class Pieces(Checkerboard):
+    player = HumanPlayer('o','O')
+    computer = ComputerPlayer('x','X')
+
+   
+
+    def setPieces(self):
+        for i in range(16):
+            if i // 8 % 2 == 0:
+                if i%2==0: self.board[i]=self.computer.getPiece()
+            elif i // 8 % 2 == 1:
+                if i%2==1: self.board[i]=self.computer.getPiece()
+
+        for i in range(48, 64):
+            if i // 8 % 2 == 0:
+                if i%2==0: self.board[i]=self.player.getPiece()
+            elif i // 8 % 2 == 1:
+                if i%2==1: self.board[i]=self.player.getPiece()
+
+    
