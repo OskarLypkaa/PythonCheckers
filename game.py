00fe401df913012b9eadd02181f234b102746319
Oskar <oskar.lypka@gmail.com>
2023-05-08 21:26:44 +0200

    Testing new inplementation on game.py

--- game.py
+++ game.py
@@ -1,14 +1,20 @@
 from board import Checkerboard
-
-
+from players import HumanPlayer, ComputerPlayer
+from pieces import Pieces
  
-# Initialization of object
-board = Checkerboard()
+# Initialization of objects
 
+board = Checkerboard()
+piece = Pieces()
+
+# Setting up pieces
+piece.setPieces()
 
 # Main logic of game in endless loop
 while True:
-    board.move(player)
     board.printBoard()
-    board.move(computer)
-    board.printBoard()
+    input()
+
+     
+    #ogólnie to co teraz próbowałem zdziałać to żeby wyświetlało którową jako dużą literę i to pogrubioną
+    
