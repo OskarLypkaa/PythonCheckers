fa316f64bb186234610b5d6e1de7abb496f32630
Oskar <oskar.lypka@gmail.com>
2023-05-08 21:27:31 +0200

    Creating basic logic for players

--- players.py
+++ players.py
@@ -1,8 +1,29 @@
+from colorama import init, Fore, Back, Style
+
 class Players():
-    pass
+    # Initialization of colorama packet
+    init()
+
+    def __init__(self, piece, queen):
+        self.piece = piece
+        self.queen = queen
+
+    def getPiece(self):
+        return self.piece
+    
+    def getQueen(self):
+        return self.queen
+    # Pamiętać, że to nie do końca działa
+    def printPawn(self, boardNumber):
+        print(Back.WHITE + Style.BRIGHT +self.board[boardNumber], end=" " + Fore.RESET)
+    
+    def printQueen(self, boardNumber):
+        print(Back.WHITE + Style.BRIGHT + self.board[boardNumber], end=" " + Fore.RESET)
 
 class HumanPlayer(Players):
-    pass
+    def __init__(self, piece, queen):
+        super().__init__(piece, queen)
 
 class ComputerPlayer(Players):
-    pass
+    def __init__(self, piece, queen):
+        super().__init__(piece, queen)
