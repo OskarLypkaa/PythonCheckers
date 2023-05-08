148bdfcebd279716cb69f2d37cde40f671213a50
Oskar <oskar.lypka@gmail.com>
2023-05-08 19:52:21 +0200

    First display of a board

---
+++ board.py
@@ -0,0 +1,53 @@
+import os
+import math
+from colorama import init, Fore, Back, Style
+
+class Checkerboard():
+
+    # Initialization of colorama packet
+    init()
+
+    # Initializaton of arguments
+    def __init__(self, board = [" "]*64):
+        self.board = board
+        
+
+    # Basic methode for printing board
+    def printBoard(self, highlited=['']):
+        # Clearing the screen
+        os.system('cls')
+        print('Checkers Game!\n'+Style.BRIGHT)
+        
+        # Printing borders surrounding board
+        self.printBorder()
+        # Listing true each element of board element in order to display it properly 
+        for i in range(64):
+            # If statment responsible for printing border in sides of board
+            if (i + 1) % 8 == 1: print(Back.MAGENTA +  str(abs(math.floor((i+1)/8)-8)), end=" " + Fore.RESET)
+
+            # Logic responsible for displaying the board in correct pattern
+            if not i in highlited:                   
+                if i // 8 % 2 == 0:
+                    if i % 2 == 0:print(Back.WHITE + self.board[i], end=" " + Fore.RESET)
+                    else:print(Back.CYAN + self.board[i], end=" " + Fore.RESET)
+                elif i // 8 % 2 == 1:
+                    if i % 2 == 0:print(Back.CYAN + self.board[i], end=" " + Fore.RESET)
+                    else:print(Back.WHITE + self.board[i], end=" " + Fore.RESET)                    
+            else: 
+                if highlited[0]==i :print(Back.RED + self.board[i], end=" " + Fore.RESET)
+                else: print(Back.GREEN + self.board[i], end=" " + Fore.RESET)
+
+            # If statment responsible for printing border in sides of board
+            if (i + 1) % 8 == 0 and not i == 0:print(Back.MAGENTA + str(abs(math.floor((i+1)/8)-9))+' ' + Fore.RESET)
+
+        # Printing borders surrounding board
+        self.printBorder()
+        # Resetting terminal color back to black
+        print(Back.BLACK + Fore.RESET)
+
+    def printBorder(self):
+        print(Back.MAGENTA + ' ', end=" " + Fore.RESET)
+        # Printing letters form asci code 
+        for i in range(97,105):     
+            print(Back.MAGENTA + chr(i), end=" " + Fore.RESET)
+        print(Back.MAGENTA + '  ' + Fore.RESET)
