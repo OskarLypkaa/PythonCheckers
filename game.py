716d033f0c1421c3f19309759c90e06032070152
Oskar <oskar.lypka@gmail.com>
2023-05-11 16:35:34 +0200

    Adding movement system for a pawns

--- game.py
+++ game.py
@@ -1,16 +1,19 @@
 from board import Checkerboard
 from pieces import Pieces
-from players import HumanPlayer
+from players import HumanPlayer, ComputerPlayer
 # Initialization of objects
 
 board = Checkerboard()
 piece = Pieces()
 player = HumanPlayer('o','O')
+computer = ComputerPlayer('x','X')
 # Setting up pieces
 piece.setPieces()
 
 # Main logic of game in endless loop
 while True:
     board.printBoard()
-    player.move()
+    player.makeAMove()
+    board.printBoard()
+    computer.makeAMove()
 
--- players.py
+++ players.py
@@ -1,3 +1,4 @@
+import random
 import time
 from board import Checkerboard
 
@@ -14,23 +15,6 @@ class Players(Checkerboard):
     def getQueen(self):
         return self.queen
 
-    def move(self):
-        try:
-            # Geting inputs of which pawn to move and which square to go
-            print("Choose pawn: ", end=" ")
-            pawn=input()
-            pawn = self.calculatePosition(pawn)
-            self.checkIfPawnIsLegal(pawn)
-
-            print("Choose move: ", end=" ")
-            move=input()
-            move = self.calculatePosition(move)
-            self.checkIfMoveIsLegal(move)
-
-        except:
-            print("Incorrect move!")          
-            time.sleep(2) 
-
     # Calculating position of choosen square. Changing e.g. c2 to 57 which is id of a board
     def calculatePosition(self, pawn):
         col = ord(pawn[:1])-96
@@ -43,43 +27,113 @@ class Players(Checkerboard):
             return True 
         else: raise ValueError
 
-    def checkIfMoveIsLegal(self, move, pawn):
-        possibleMoves=self.rules(pawn, self.board)
-        if move in possibleMoves:
+    def checkIfMoveIsLegal(self, move, moves):
+        if move in moves:
             return True
         else: raise ValueError
 
-    def rules(self, chosenPiece, cheakBaord, comp='X',deep=0):
+    # This methode wont work, it has to be corrected
+    def killPawn(self, pawn):
+        self.board[pawn] = ' '
+
+    # Core for game movement, it contains rules of pawn movement calculated based on recuration
+    def rules(self, chosenPiece, cheakBaord, deep=0):
         possibleMove=[]
-        if comp == 'X':
+        if isinstance(self, HumanPlayer):
             directions = [-7, -9]
             jumps = [-14, -18]
             limit = [7 , 15]
-        else:
-            directions = [7, 9]
-            jumps = [14, 18]
-            limit = [7 , 15]
-
+            piece = 'x'
+        elif isinstance(self, ComputerPlayer):
+            directions = [9, 7]
+            jumps = [18, 14]
+            limit = [48 , 56]
+            piece = 'o'
                 
-        if deep==0 and chosenPiece > limit[0]:
+        if deep==0 and self.movingLimit(chosenPiece, limit):
             if chosenPiece + directions[0] % 8 != 0 and cheakBaord[chosenPiece+directions[0]]==' ':
                 possibleMove.append(chosenPiece+directions[0])
             if chosenPiece % 8 != 0 and cheakBaord[chosenPiece+directions[1]]==' ':
                 possibleMove.append(chosenPiece+directions[1])
-        if chosenPiece > limit[1]:
-            if chosenPiece + jumps[0] % 8 != 0 and cheakBaord[chosenPiece+directions[0]].upper() == comp and cheakBaord[chosenPiece+jumps[0]] == ' ':               
+        if self.movingLimit(chosenPiece, limit, jump=True):
+            if chosenPiece + jumps[0] % 8 != 0 and cheakBaord[chosenPiece+directions[0]].upper() == piece.upper() and cheakBaord[chosenPiece+jumps[0]] == ' ':               
                 possibleMove.append(chosenPiece+jumps[0])
+                self.killPawn(chosenPiece+directions[0])
                 possibleMove.extend(self.rules(chosenPiece+jumps[0], cheakBaord, deep=deep+1))
-            if chosenPiece % 8 != 0 and cheakBaord[chosenPiece+directions[1]].upper() == comp and cheakBaord[chosenPiece+jumps[1]] == ' ':
+            if chosenPiece % 8 != 0 and cheakBaord[chosenPiece+directions[1]].upper() == piece.upper() and cheakBaord[chosenPiece+jumps[1]] == ' ':
                 possibleMove.append(chosenPiece+jumps[1])
+                self.killPawn(chosenPiece+directions[0])
                 possibleMove.extend(self.rules(chosenPiece+jumps[1], cheakBaord, deep=deep+1))
         return possibleMove
 
+    # Checking if pawn is close to the end of a board in order to prevent errors
+    def movingLimit(self, piece, limit, jump=False):
+        if isinstance(self, HumanPlayer):
+            if not jump:
+                if piece > limit[0]: return True
+            else:
+                if piece > limit[1]: return True
+            return False
+        elif isinstance(self, ComputerPlayer):
+            if not jump:
+                if piece < limit[0]: return True
+            else:
+                if piece < limit[1]: return True
+            return False
 
 class HumanPlayer(Players):
     def __init__(self, piece, queen):
         super().__init__(piece, queen)
 
+    def makeAMove(self):
+        try:
+            # Geting inputs of which pawn to move
+            print("Choose pawn: ", end=" ")
+            pawn=input()
+            pawn = self.calculatePosition(pawn)
+            self.checkIfPawnIsLegal(pawn)
+
+            # Highlighting possible moves
+            highlighted = []
+            highlighted.append(pawn)
+            highlighted.extend(self.rules(pawn, self.board))
+            # Printing board again but with highlighted moves
+            self.printBoard(highlighted)
+
+            # Geting inputs of which square to go
+            print("Choose move: ", end=" ")
+            move=input()
+            move = self.calculatePosition(move)
+            self.checkIfMoveIsLegal(move, highlighted)
+            
+            # Finally moving a piece
+            self.move(move, pawn)
+        except:
+            print("Incorrect move!")          
+            time.sleep(2) 
+
+    # Deleting old position of piece and placing it in a new, chosen one
+    def move(self, move, pawn, isaQueen=None):
+        self.board[pawn]=' '
+        if isaQueen:self.board[move]=self.queen
+        else: self.board[move]=self.piece
+        
 class ComputerPlayer(Players):
     def __init__(self, piece, queen):
         super().__init__(piece, queen)
+
+    # Begining of an AI, now computer is listing all of the possible moves and chosing it randomly
+    def makeAMove(self):
+        while True:
+            random_pawn = random.randint(0, 63)
+            if self.board[random_pawn] == self.piece or self.board[random_pawn] == self.queen:
+                possible_moves = self.rules(random_pawn, self.board)
+                if possible_moves: break
+        new_pos = random.choice(possible_moves)
+        self.move(new_pos, random_pawn)
+
+    # Deleting old position of piece and placing it in a new, chosen one
+    def move(self, move, pawn, isaQueen=None):
+        self.board[pawn]=' '
+        if isaQueen:self.board[move]=self.queen
+        else: self.board[move]=self.piece
